627f0e578c77d8d98cf3ff3b5f657cc0b0999c5e
CoderUnknownNL <quincyommel@icloud.com>
2026-03-24 08:04:43 +0100

    Chat.js

--- api/chat.js
+++ api/chat.js
@@ -14,7 +14,6 @@ export default async function handler(req, res) {
       },
       body: JSON.stringify(req.body)
     });
-
     const data = await response.json();
     res.status(response.status).json(data);
   } catch (err) {
